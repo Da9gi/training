a5cce10ad2e81329c693ed558abf75493786693b
“arundangi” <arunkumardangi13@gmail.com>
2020-10-14 15:53:51 +0530

    Some modification

--- src/Fibbo_Recursion.js
+++ src/Fibbo_Recursion.js
@@ -2,5 +2,5 @@ function nthFibboNo(index) {
     return (index == 0 || index == 1) ? index : (nthFibboNo(index - 1) + (nthFibboNo(index - 2)));
 }
 
-index = prompt("Enter the index ", "");
-alert(nthFibboNo(index));
+let index = 40; 
+console.log(nthFibboNo(index));
---
+++ src/test.js
@@ -0,0 +1,7 @@
+let arr=[1,2,3,4];
+// let a=arr.push(arr.shift());
+// console.log(a);
+// console.log(arr);
+
+arr.splice(1,0,9,78,76,99,007,9,6,867);
+console.log(arr)
