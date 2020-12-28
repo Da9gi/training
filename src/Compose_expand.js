340b509eb1238c7b14ef2d0f1f24c56f5ab38ad5
“arundangi” <arunkumardangi13@gmail.com>
2020-12-28 13:46:29 +0530

    D

--- src/Compose_expand.js
+++ src/Compose_expand.js
@@ -10,4 +10,25 @@ let comp = function(...fns) {
     }
 
 
+const f1 = (val) => {
+    let resArr = val.map((item) => item*3);
+    return resArr;
+}
+
+const f2 = (val) => {
+    let resArr = val.map((item) => item+2);
+    return resArr;
+}
+
+const f3 = (val) => {
+    console.log(val);
+    return val;
+}
+
+let arr = [1, 2, 3, 4];
+const res = comp(f3, f1, f2)(arr);
+console.log(res);
+
+
+
     
--- src/test.js
+++ src/test.js
@@ -1,7 +1,38 @@
-let arr=[1,2,3,4];
-// let a=arr.push(arr.shift());
-// console.log(a);
-// console.log(arr);
+function evalPossibilities(numbers) {
+    let operands = {
+        0: "+",
+        1: "-",
+        2: "",
+    };
+    let flag = "";
+    let try1 = "";
 
-arr.splice(1,0,9,78,76,99,007,9,6,867);
-console.log(arr)
+    for (let i = 0; i <= Math.pow(3, numbers.length - 1); i++) {
+        flag = ("00000000" + i.toString(3)).substr(-8, 8).split("").map((element) => parseInt(element));;
+        try1 =
+            "1" +
+            operands[flag[0]] +
+            "2" +
+            operands[flag[1]] +
+            "3" +
+            operands[flag[2]] +
+            "4" +
+            operands[flag[3]] +
+            "5" +
+            operands[flag[4]] +
+            "6" +
+            operands[flag[5]] +
+            "7" +
+            operands[flag[6]] +
+            "8" +
+            operands[flag[7]] +
+            "9";
+        
+        if (eval(try1) === 100) {
+            console.log(try1);
+        }
+    }
+}
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+evalPossibilities(numbers);
